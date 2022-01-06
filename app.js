62561d428b6116d767c0b61be2f98d38e7b4e80b
Tozinoo <>
2022-01-06 12:30:34 +0900

    로그인, 회원가입 구현
    db 비밀번호 수정 필

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { sequelize } = require("./models/index.js");
+
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+// 라우터 선언
+const indexRouter = require("./routers/index.js");
+const usersRouter = require("./routers/users.js");
+const signinRouter = require("./routers/signin.js");
+const signupRouter = require("./routers/signup.js");
+
+const app = express();
+
+// DB와 연결
+sequelize
+    // sync : MySQL에 테이블이 존재 하지 않을때 생성
+    //      force: true   => 이미 테이블이 있으면 drop하고 다시 테이블 생성
+    .sync({ force: false })
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+// PORT setting
+const PORT = 80;
+app.set("port", process.env.PORT || PORT);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// req.session 객체 생성
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+        // redis저장 설정
+        store: new RedisStore({ client: redisClient }),
+    })
+);
+
+// URL과 라우터 매칭
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/signin", signinRouter);
+app.use("/signup", signupRouter);
+
+// ERROR 메세지 창
+app.use((err, req, res, next) => {
+    res.status(err.static || 500);
+    res.send(err);
+});
+
+// PORT 연결상태 확인
+app.listen(app.get("port"), () =>
+    console.log(`Listening on port ${app.get("port")}`)
+);
---
+++ httpServer.js
@@ -0,0 +1,118 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { sequelize } = require("./models/index.js");
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+// 라우터 선언
+const indexRouter = require("./routers/index.js");
+const authRouter = require("./routers/auth");
+
+// 블록체인 관련
+const { getBlocks, nextBlock, getVersion } = require("./chainedBlock.js");
+const { addBlock } = require("./checkValidBlock");
+const { connectToPeers, getSockets } = require("./p2pServer.js");
+const { getPublicKeyFromWallet, initWallet } = require("./encryption");
+
+const http_port = process.env.HTTP_PORT || 3001;
+
+// DB와 연결
+sequelize
+    // sync : MySQL에 테이블이 존재 하지 않을때 생성
+    //      force: true   => 이미 테이블이 있으면 drop하고 다시 테이블 생성
+    .sync({ force: false })
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+function initHttpServer() {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser(process.env.COOKIE_SECRET));
+
+    // req.session 객체 생성
+    app.use(
+        session({
+            resave: false,
+            saveUninitialized: false,
+            secret: process.env.COOKIE_SECRET,
+            cookie: {
+                httpOnly: true,
+                secure: false,
+            },
+        })
+    );
+
+    // URL과 라우터 매칭
+    app.use("/", indexRouter);
+    app.use("/auth", authRouter);
+
+    //추가
+    app.post("/addPeers", (req, res) => {
+        const data = req.body.data || [];
+        console.log(data);
+        connectToPeers(data);
+        res.send(data);
+    });
+    app.get("/peers", (req, res) => {
+        let sockInfo = [];
+
+        getSockets().forEach((s) => {
+            sockInfo.push(s._socket.remoteAddress + ":" + s._socket.remotePort);
+        });
+        res.send(sockInfo);
+    });
+
+    app.get("/blocks", (req, res) => {
+        res.send(getBlocks());
+    });
+    app.get("/version", (req, res) => {
+        res.send(getVersion());
+    });
+
+    app.post("/mineBlock", (req, res) => {
+        const data = req.body.data || [];
+        console.log(data);
+        const block = nextBlock(data);
+        addBlock(block);
+        // res.send(block)
+        res.send(getBlocks());
+    });
+
+    app.post("/stop", (req, res) => {
+        res.send({ msg: "Stop Server!" });
+        process.exit();
+    });
+    app.get("/address", (req, res) => {
+        initWallet();
+        const address = getPublicKeyFromWallet().toString();
+        console.log(getPublicKeyFromWallet());
+        if (address != "") {
+            res.send({ address: address });
+        } else {
+            res.send("empty address!");
+        }
+    });
+
+    // ERROR 메세지 창
+    app.use((err, req, res, next) => {
+        res.status(err.static || 500);
+        res.send(err);
+    });
+
+    app.listen(http_port, () => {
+        console.log("Listening Http Port : " + http_port);
+    });
+}
+
+initHttpServer();
