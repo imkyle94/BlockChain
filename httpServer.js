07ec649aa592473fe9c4f5401424e1c961715a3a
Park <andy3638@naver.com>
2022-01-11 17:52:22 +0900

    설명 추가

--- httpServer.js
+++ httpServer.js
@@ -31,121 +31,124 @@ const http_port = process.env.HTTP_PORT || 3001;
 
 // DB와 연결
 sequelize
-    // sync : MySQL에 테이블이 존재 하지 않을때 생성
-    //      force: true   => 이미 테이블이 있으면 drop하고 다시 테이블 생성
-    .sync({ force: false })
-    .then(() => {
-        console.log("Database connected successfully");
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+  // sync : MySQL에 테이블이 존재 하지 않을때 생성
+  //      force: true   => 이미 테이블이 있으면 drop하고 다시 테이블 생성
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 function initHttpServer() {
-    const app = express();
+  const app = express();
 
-    // build 폴더 지정
-    app.use("/", express.static(path.join(__dirname, "./build")));
+  // build 폴더 지정
+  app.use("/", express.static(path.join(__dirname, "./build")));
 
-    app.use(bodyParser.json());
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(bodyParser.json());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser(process.env.COOKIE_SECRET));
 
-    app.use(
-        expressCspHeader({
-            directives: {
-                "script-src": [
-                    SELF,
-                    INLINE,
-                    "https://cdnjs.cloudflare.com",
-                    "https://unpkg.com",
-                ],
-            },
-        })
-    );
+  app.use(
+    expressCspHeader({
+      directives: {
+        "script-src": [
+          SELF,
+          INLINE,
+          "https://cdnjs.cloudflare.com",
+          "https://unpkg.com",
+        ],
+      },
+    })
+  );
 
-    // req.session 객체 생성
-    app.use(
-        session({
-            resave: false,
-            saveUninitialized: false,
-            secret: process.env.COOKIE_SECRET,
-            cookie: {
-                httpOnly: true,
-                secure: false,
-            },
-        })
-    );
+  // req.session 객체 생성
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+      },
+    })
+  );
 
-    // passport setting
-    passportConfig();
-    // passport 설정 선언(req에 passport 설정 삽입) 위 use.session이라고 보면 댐
-    app.use(passport.initialize());
-    // req.session 에 passport 정보 저장 (req.session.num = 1 이런거라고 보면 댐)
-    app.use(passport.session());
+  // passport setting
+  passportConfig();
+  // passport 설정 선언(req에 passport 설정 삽입) 위 use.session이라고 보면 댐
+  app.use(passport.initialize());
+  // req.session 에 passport 정보 저장 (req.session.num = 1 이런거라고 보면 댐)
+  app.use(passport.session());
 
-    // URL과 라우터 매칭
-    app.use("/", indexRouter);
-    app.use("/auth", authRouter);
-    app.use(otherRouter);
+  // URL과 라우터 매칭
+  app.use("/", indexRouter);
+  app.use("/auth", authRouter);
+  app.use(otherRouter);
 
-    //추가
-    app.post("/addPeers", (req, res) => {
-        const data = req.body.data || [];
-        console.log(data);
-        connectToPeers(data);
-        res.send(data);
+  //추가
+  // 보통은 wss, ws 두개를 구분 짓겠지만
+  // 여기서 ws 익스포트한 함수를 불러오네
+  app.post("/addPeers", (req, res) => {
+    //   여기 data가 ws에서 연결 주소를 나타낸다
+    const data = req.body.data || [];
+    console.log(data);
+    connectToPeers(data);
+    res.send(data);
+  });
+  app.get("/peers", (req, res) => {
+    let sockInfo = [];
+
+    getSockets().forEach((s) => {
+      sockInfo.push(s._socket.remoteAddress + ":" + s._socket.remotePort);
     });
-    app.get("/peers", (req, res) => {
-        let sockInfo = [];
+    res.send(sockInfo);
+  });
 
-        getSockets().forEach((s) => {
-            sockInfo.push(s._socket.remoteAddress + ":" + s._socket.remotePort);
-        });
-        res.send(sockInfo);
-    });
+  app.get("/blocks", (req, res) => {
+    res.send(getBlocks());
+  });
+  app.get("/version", (req, res) => {
+    res.send(getVersion());
+  });
 
-    app.get("/blocks", (req, res) => {
-        res.send(getBlocks());
-    });
-    app.get("/version", (req, res) => {
-        res.send(getVersion());
-    });
+  app.post("/mineBlock", (req, res) => {
+    const data = req.body.data || [];
+    console.log(data);
+    const block = nextBlock(data);
+    addBlock(block);
+    // res.send(block)
+    res.send(getBlocks());
+  });
 
-    app.post("/mineBlock", (req, res) => {
-        const data = req.body.data || [];
-        console.log(data);
-        const block = nextBlock(data);
-        addBlock(block);
-        // res.send(block)
-        res.send(getBlocks());
-    });
+  app.post("/stop", (req, res) => {
+    res.send({ msg: "Stop Server!" });
+    process.exit();
+  });
+  app.get("/address", (req, res) => {
+    initWallet();
+    const address = getPublicKeyFromWallet().toString();
+    console.log(getPublicKeyFromWallet());
+    if (address != "") {
+      res.send({ address: address });
+    } else {
+      res.send("empty address!");
+    }
+  });
 
-    app.post("/stop", (req, res) => {
-        res.send({ msg: "Stop Server!" });
-        process.exit();
-    });
-    app.get("/address", (req, res) => {
-        initWallet();
-        const address = getPublicKeyFromWallet().toString();
-        console.log(getPublicKeyFromWallet());
-        if (address != "") {
-            res.send({ address: address });
-        } else {
-            res.send("empty address!");
-        }
-    });
+  // ERROR 메세지 창
+  app.use((err, req, res, next) => {
+    res.status(err.static || 500);
+    res.send(err);
+  });
 
-    // ERROR 메세지 창
-    app.use((err, req, res, next) => {
-        res.status(err.static || 500);
-        res.send(err);
-    });
-
-    app.listen(http_port, () => {
-        console.log("Listening Http Port : " + http_port);
-    });
+  app.listen(http_port, () => {
+    console.log("Listening Http Port : " + http_port);
+  });
 }
 
 initHttpServer();
--- p2pServer.js
+++ p2pServer.js
@@ -5,33 +5,42 @@ const WebSocket = require("ws");
 const { createHash, addBlock } = require("./chainedBlock");
 // const { getLastBlock } = require("./chainedBlock")
 
+// 함수 나열을 해봅시다
+// (가져다 쓰게, 보기 쉽게, 통일성 주게, 기능 별로, 행위를 표현하기 위해)
+
+// 봐야할 것은 개발 환경 조차 매개변수 처리를 해 활용이 가능하게 미리 사전에 구성한 것.
+// 이거 내가 사전처리를 해야겠다고 말했던 부분 좋은 예제
+
 //p2p서버 초기화한는 하뭇
 function initP2PServer(test_port) {
-    const server = new WebSocket.Server({ port: test_port });
-    server.on("connection", (ws) => {
-        initConnection(ws);
-    });
-    console.log("Listening webSocket port : " + test_port);
+  const server = new WebSocket.Server({ port: test_port });
+  server.on("connection", (ws) => {
+    initConnection(ws);
+  });
+  console.log("Listening webSocket port : " + test_port);
 }
 
 initP2PServer(6001);
 // initP2PServer(6002)
 initP2PServer(6003);
 
+// 여기 sockets에 뭐가 들어가닝?
 let sockets = [];
 
+// 한번 봐보니, 메세지 받는 처리 등을 넣어놨겠지
 function initConnection(ws) {
-    sockets.push(ws);
-    initMessageHandler(ws);
-    initErrorHandler(ws);
+  sockets.push(ws);
+  initMessageHandler(ws);
+  initErrorHandler(ws);
 }
 function getSockets() {
-    return sockets;
+  return sockets;
 }
 
 //메세지를 제이슨 형태로 전달 : 내가 가지고 있는 블록체인이 올바르지 않다, 너꺼 줘봐 해서 비교하고 내꺼가 틀리면 교체 등.
+// 다시 클라로 쏴주는 함수네 보니
 function write(ws, message) {
-    ws.send(JSON.stringify(message));
+  ws.send(JSON.stringify(message));
 }
 
 // for (let i = 0; i < 10.; i++) {
@@ -39,130 +48,131 @@ function write(ws, message) {
 // }
 
 function broadcast(message) {
-    // function (socket) {
-    //   write(socket,message)
-    // }
-    ///////////////////////
-    //위의 소켓함수와 같은 말
-    sockets.forEach((socket) => {
-        write(socket, message);
-    });
+  // function (socket) {
+  //   write(socket,message)
+  // }
+  ///////////////////////
+  //위의 소켓함수와 같은 말
+  sockets.forEach((socket) => {
+    write(socket, message);
+  });
 }
 
 function connectToPeers(newPeers) {
-    newPeers.forEach((peer) => {
-        const ws = new WebSocket(peer);
-        console.log(ws);
-        ws.on("open", () => {
-            console.log("open");
-            initConnection(ws);
-        });
-        ws.on("error", (errorType) => {
-            console.log("connection Failed!") + errorType;
-        });
+  newPeers.forEach((peer) => {
+    const ws = new WebSocket(peer);
+    console.log(ws);
+    ws.on("open", () => {
+      console.log("open");
+      initConnection(ws);
     });
+    ws.on("error", (errorType) => {
+      console.log("connection Failed!") + errorType;
+    });
+  });
 }
 
 //message handler
 const MessageType = {
-    //내가 가지고 있는데 가장 최신블록을 담아서 보내줌
-    QUERY_LATEST: 0,
-    //내가 가지고 있는데이터필드 전체에 블록을 담아서 보낼때
-    QUERY_ALL: 1,
-    //데이터필드에 하나 이상의 블록이 있을 때 회신할때는 이타입으로
-    RESPONSE_BLOCKCHAIN: 2,
+  //내가 가지고 있는데 가장 최신블록을 담아서 보내줌
+  QUERY_LATEST: 0,
+  //내가 가지고 있는데이터필드 전체에 블록을 담아서 보낼때
+  QUERY_ALL: 1,
+  //데이터필드에 하나 이상의 블록이 있을 때 회신할때는 이타입으로
+  RESPONSE_BLOCKCHAIN: 2,
 };
 
+// 메세지 받는 처리 두둥
 function initMessageHandler(ws) {
-    ws.on("message", (data) => {
-        const message = JSON.parse(data);
-        switch (message.type) {
-            case MessageType.QUERY_LATEST:
-                write(ws, responseLastestMsg());
-                break;
-            case MessageType.QUERY_ALL:
-                write(ws, responseAllChainMsg());
-                break;
-            case MessageType.RESPONSE_BLOCKCHAIN:
-                handleBlockChainResponse(message);
-                break;
-        }
-    });
+  ws.on("message", (data) => {
+    const message = JSON.parse(data);
+
+    // 받은 메세지가 최신, 전체 등등의 유형으로 나눠서 write 함수네
+    switch (message.type) {
+      case MessageType.QUERY_LATEST:
+        write(ws, responseLastestMsg());
+        break;
+      case MessageType.QUERY_ALL:
+        write(ws, responseAllChainMsg());
+        break;
+      case MessageType.RESPONSE_BLOCKCHAIN:
+        handleBlockChainResponse(message);
+        break;
+    }
+  });
 }
 
 function responseLastestMsg() {
-    return {
-        type: RESPONSE_BLOCKCHAIN,
-        data: JSON.stringify([getLastBlock()]),
-    };
+  return {
+    type: RESPONSE_BLOCKCHAIN,
+    data: JSON.stringify([getLastBlock()]),
+  };
 }
 function responseAllChainMsg() {
-    return {
-        type: RESPONSE_BLOCKCHAIN,
-        data: JSON.stringify(getBlocks()),
-    };
+  return {
+    type: RESPONSE_BLOCKCHAIN,
+    data: JSON.stringify(getBlocks()),
+  };
 }
 function handleBlockChainResponse(message) {
-    const receiveBlocks = JSON.parse(message.data);
-    //받은것중에 마지막꺼
-    const latestReceiveBlock = receiveBlocks[receiveBlocks.length - 1];
-    const latesMyBlock = getLastBlock();
-    //데이터로 받은 블럭 중에 마지막 블럭의 인덱스가
-    //내가 보유중인 마지막 블럭의 인덱스보다 클때 / 작을 때 (작은건 사실상 필요없지)
-    if (latestReceiveBlock.header.index > latesMyBlock.header.index) {
-        //받은 마지막 블록의 이전해시값이 내 마지막 블럭일 때
-        //이런경우에는 내꺼에다가 마지막 블록을 추가해주면 됨
-        if (
-            createHash(latesMyBlock) === latestReceiveBlock.header.previousHash
-        ) {
-            if (addBlock(latestReceiveBlock)) {
-                broadcast(responseLastestMsg());
-            } else {
-                console.log("Invalid Block");
-            }
-        }
-        //받은 블럭의 전체 크기가 1일때, 블록 전체 다시 달라고요청
-        else if (receiveBlocks.length === 1) {
-            broadcast(queryAllMsg());
-        }
-        //아닐때는 내 전체 블록이 다른블록보다 동기화가 안된 상황
-        //지금 받은 걸로 통채를 갈아끼워야함.
-        else {
-            replaceChain(receiveBlocks);
-        }
-    } else {
-        console.log("Do nothing.");
+  const receiveBlocks = JSON.parse(message.data);
+  //받은것중에 마지막꺼
+  const latestReceiveBlock = receiveBlocks[receiveBlocks.length - 1];
+  const latesMyBlock = getLastBlock();
+  //데이터로 받은 블럭 중에 마지막 블럭의 인덱스가
+  //내가 보유중인 마지막 블럭의 인덱스보다 클때 / 작을 때 (작은건 사실상 필요없지)
+  if (latestReceiveBlock.header.index > latesMyBlock.header.index) {
+    //받은 마지막 블록의 이전해시값이 내 마지막 블럭일 때
+    //이런경우에는 내꺼에다가 마지막 블록을 추가해주면 됨
+    if (createHash(latesMyBlock) === latestReceiveBlock.header.previousHash) {
+      if (addBlock(latestReceiveBlock)) {
+        broadcast(responseLastestMsg());
+      } else {
+        console.log("Invalid Block");
+      }
     }
+    //받은 블럭의 전체 크기가 1일때, 블록 전체 다시 달라고요청
+    else if (receiveBlocks.length === 1) {
+      broadcast(queryAllMsg());
+    }
+    //아닐때는 내 전체 블록이 다른블록보다 동기화가 안된 상황
+    //지금 받은 걸로 통채를 갈아끼워야함.
+    else {
+      replaceChain(receiveBlocks);
+    }
+  } else {
+    console.log("Do nothing.");
+  }
 }
 
 function queryAllMsg() {
-    return {
-        type: QUERY_ALL,
-        data: null,
-    };
+  return {
+    type: QUERY_ALL,
+    data: null,
+  };
 }
 function queryLatestMsg() {
-    return {
-        type: QUERY_LATEST,
-        data: null,
-    };
+  return {
+    type: QUERY_LATEST,
+    data: null,
+  };
 }
 
 //테스트하려면 서버 2개 열어서 요청하고 받은걸 타입들이 오는지 확인..
 
 function initErrorHandler(ws) {
-    ws.on("close", () => {
-        closeConnection(ws);
-    });
-    ws.on("error", () => {
-        closeConnection(ws);
-    });
+  ws.on("close", () => {
+    closeConnection(ws);
+  });
+  ws.on("error", () => {
+    closeConnection(ws);
+  });
 }
 
 function closeConnection(ws) {
-    console.log(`Connection close ${ws.url}`);
-    //splice는 소켓을 복사해서 뒤에 넣음 초기화한다고 생각
-    sockets.splice(sockets.indexOf(ws), 1);
+  console.log(`Connection close ${ws.url}`);
+  //splice는 소켓을 복사해서 뒤에 넣음 초기화한다고 생각
+  sockets.splice(sockets.indexOf(ws), 1);
 }
 
 module.exports = { connectToPeers, getSockets };
